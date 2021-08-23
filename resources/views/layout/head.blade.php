4263e545846b1e4b2104d1d8ba56ab4d4d789711
ogirians <ogi.rians3@gmail.com>
2021-08-23 09:09:32 +0700

    head blade from hosting

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -122,5 +122,17 @@ $site = DB::table('konfigurasi')->first();
   </style>
 <?php echo $site->metatext; ?>
 </head>
-
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/60fe6a05649e0a0a5ccdf198/1fbgss9kk';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+<!--End of Tawk.to Script-->
 <body>
